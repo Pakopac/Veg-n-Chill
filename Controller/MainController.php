7556baa491aac4dc5ab18fb6b6594e5916faf88c
Pakopac <lilian.pacaud@supinternet>
2018-03-26 12:48:22 +0200

    Added register

--- Controller/MainController.php
+++ Controller/MainController.php
@@ -1,11 +1,42 @@
 <?php
 
 require_once('Cool/BaseController.php');
+require_once('Model/UserManager.php');
 
 class MainController extends BaseController
 {
     public function homeAction()
     {
         return $this->render('home.html.twig');
+    }
+    public function registerAction()
+    {
+        if (isset($_SESSION['pseudo'])){
+            $this->redirectToRoute('home');
+        }
+        else {
+            if (!empty($_POST['firstname']) && !empty($_POST['lastname'])
+                && !empty($_POST['pseudo']) && !empty($_POST['email'])
+                && !empty($_POST['password']) && !empty($_POST['repeatPassword'])) {
+                $firstname = htmlentities($_POST['firstname']);
+                $lastname = htmlentities($_POST['lastname']);
+                $pseudo = htmlentities($_POST['pseudo']);
+                $email = htmlentities($_POST['email']);
+                $password = $_POST['password'];
+                $repeatPassword = $_POST['repeatPassword'];
+
+                $UserManager = new UserManager();
+                $errors = $UserManager->registerUser($firstname, $lastname, $pseudo, $email, $password, $repeatPassword);
+                if ($errors === []) {
+                    $data = ['user' => $_SESSION];
+                    $this->redirectToRoute('home');
+                    return $this->render('layout.html.twig', $data);
+                } else {
+                    $data = ['errors' => $errors];
+                    return $this->render('register.html.twig', $data);
+                }
+            }
+            return $this->render('register.html.twig');
+        }
     }
 }
---
+++ Model/UserManager.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once('Cool/DBManager.php');
+
+class UserManager
+{
+    public function registerUser($firstname, $lastname, $pseudo, $email,$password, $repeatPassword)
+    {
+        $regexPassword = "\"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$\"";
+        $regexEmail =  " /^[^\W][a-zA-Z0-9]+(.[a-zA-Z0-9]+)@[a-zA-Z0-9]+(.[a-zA-Z0-9]+)*.[a-zA-Z]{2,4}$/ ";
+        $errors = [];
+
+        if (!preg_match($regexEmail,$email)){
+            $errors[] = 'Invalid Email';
+        }
+        if((strlen($pseudo) < 4) || (strlen($pseudo) > 20)){
+            $errors[] = 'Pseudo too short or too long';
+        }
+        if(!preg_match($regexPassword,$password)){
+            $errors[] = 'Password must have at least 6 characters with 1 letter uppercase and 1 number';
+        }
+        if($password !== $repeatPassword){
+            $errors[] = 'Password must be identical to the verification';
+        }
+        if($errors === []) {
+            $dbm = DBManager::getInstance();
+            $pdo = $dbm->getPdo();
+            $hashedPwd = password_hash($password, PASSWORD_BCRYPT);
+
+            $stmt = $pdo->prepare("INSERT INTO `users` (`id`, `pseudo`, `password`, `firstname`, `lastname`, `email`) VALUES (NULL, :pseudo, :password, :firstname, :lastname, :email)");
+            $stmt->bindParam(':pseudo', $pseudo);
+            $stmt->bindParam(':password', $hashedPwd);
+            $stmt->bindParam(':firstname', $firstname);
+            $stmt->bindParam(':lastname', $lastname);
+            $stmt->bindParam(':email', $email);
+
+            $stmt->execute();
+        }
+        return $errors;
+    }
+}
