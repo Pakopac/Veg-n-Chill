d9c996436d8611ee0b39f452975b16fafb86a547
Pakopac <lilian.pacaud@supinternet>
2018-05-25 22:52:56 +0200

    Added food Addict page

--- Controller/MainController.php
+++ Controller/MainController.php
@@ -191,4 +191,11 @@ class MainController extends BaseController
         ];
         return $this -> render('interview.html.twig', $arr);
     }
+    public function foodAddictAction()
+    {
+        $arr = [
+            'user' => $_SESSION
+        ];
+        return $this -> render('foodAddict.html.twig', $arr);
+    }
 }
